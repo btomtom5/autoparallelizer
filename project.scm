8310fe40b427c6c396cf4ee41871365eac00d01b
David <david@localhost>
2017-05-09 17:55:58 -0400

    bfs order for sched traversal which fixes numbering bug

--- project.scm
+++ project.scm
@@ -1,3 +1,4 @@
+
 (define program '(+ (- 4 6) (/ 5 (* 2 (- 3 4) (* 5 6))) (+ 7 (* 4 5 (- 4 6)))))
 ;Value: program
 
@@ -23,46 +24,48 @@
 
 
 
-(define (number-deps program sched parent)
-  (let loop ((spot (cdr program)) (re-def `(,(car program))) (count 0))
-    (if (pair? (car spot))
-	(begin
-	  (append! re-def `((get-dep ,(length sched))))
-	  (set! sched (number-deps (car spot) sched (+ (length sched) (calc-deps program))))
-	  (set! count (+ 1 count)))
-	(append! re-def `(,(car spot))))
-    (if (= (length spot) 1)
-	(begin
-	  (set! sched (append! sched `(,(cons re-def (cons count parent)))))
-	  sched)
-	(loop (cdr spot) re-def count))))
+(define (number-deps sched queue)
+  (let qloop ((queue queue)) 
+    (if (= (length queue) 0)
+	sched
+	(let ((program (car (car queue)))) 
+	  (let loop ((spot (cdr program)) (re-def `(,(car program))) (count 0))
+	    (if (pair? (car spot))
+		(begin
+		  (append! re-def `((get-dep ,(+ (length sched) (length queue)))))
+		  (set! queue (append! queue `(,(cons (car spot) (length sched)))))
+		  (set! count (+ 1 count)))
+		(append! re-def `(,(car spot))))
+	    (if (= (length spot) 1)
+		(begin
+		  (set! sched (append! sched `(,(cons re-def (cons count (cdr (car queue)))))))
+		  (qloop (cdr queue)))
+		(loop (cdr spot) re-def count)))))))
 ;Value: number-deps
 
 
-(number-deps simple-prog '() #f)
+(define (make-sched program)
+  (number-deps '() `((,program #f))))
+;Value: make-sched
+
+
+(pp (make-sched simple-prog))
 (
- ((* 4 5) 0 . 1)
- ((+ (get-dep 0) 1) 1 . #f)
+ ((+ (get-dep 1) 1) 1 #f) 
+ ((* 4 5) 0 . 0)
 )
 
-(pp (number-deps program '() #f))
+
+(pp (make-sched program))
 (
- ((- 4 6) 0 . 3) 
- ((- 3 4) 0 . 3)
+ ((+ (get-dep 1) (get-dep 2) (get-dep 3)) 3 #f) 
+ ((- 4 6) 0 . 0)
+ ((/ 5 (get-dep 4)) 1 . 0)
+ ((+ 7 (get-dep 5)) 1 . 0)
+ ((* 2 (get-dep 6) (get-dep 7)) 2 . 2)
+ ((* 4 5 (get-dep 8)) 1 . 3)
+ ((- 3 4) 0 . 4)
  ((* 5 6) 0 . 4)
- ((* 2 (get-dep 1) (get-dep 2)) 2 . 2)
- ((/ 5 (get-dep 1)) 1 . 4)
- ((- 4 6) 0 . 6)
- ((* 4 5 (get-dep 5)) 1 . 6)
- ((+ 7 (get-dep 5)) 1 . 8)
- ((+ (get-dep 0) (get-dep 1) (get-dep 5)) 3 . #f)
+ ((- 4 6) 0 . 5)
 )
-
-
-
-
-
-
-
-
 
